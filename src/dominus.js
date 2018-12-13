4f3fc3785e1aff002f642aa15c12ae359a8fae7f
Gurigraphics <gurigraphics@gmail.com>
2018-12-13 00:46:17 -0200

    Change "type" by "tag"

--- src/dominus.js
+++ src/dominus.js
@@ -1,6 +1,6 @@
 /*
  * 	DOMinus.js
- *	Version 0.1.0
+ *	Version 0.1.1
  * 	https://github.com/Gurigraphics/DOMinus.js
  *
  * 	Licensed under the MIT license:
@@ -13,77 +13,79 @@ var HTML = {}
 var DOM = {
     RENDER: [],
     TAGS: [],      
-    get: function( tag ){
-       if( DOM.TAGS[ tag ] ){ 
-         return DOM.TAGS[ tag ]  
-       }else if( UTILS.contains( tag, "." ) ) { 
-         DOM.TAGS[ tag ] = document.getElementsByClassName( UTILS.subs( tag ) )[0]
-       }else if( UTILS.contains( tag, "#" ) ){ 
-         DOM.TAGS[ tag ] = document.getElementById( UTILS.subs( tag ) ) 
+    get: function( el ){
+       if( DOM.TAGS[ el ] ){ 
+         return DOM.TAGS[ el ]  
+       }else if( UTILS.contains( el, "." ) ) { 
+         DOM.TAGS[ el ] = document.getElementsByClassName( UTILS.subs( el ) )[0]
+       }else if( UTILS.contains( el, "#" ) ){ 
+         DOM.TAGS[ el ] = document.getElementById( UTILS.subs( el ) ) 
        }else { 
-         DOM.TAGS[ tag ] = document.getElementsByTagName( tag )[0] 
+         DOM.TAGS[ el ] = document.getElementsByTagName( el )[0] 
        }    
-       return DOM.TAGS[ tag ]
+       return DOM.TAGS[ el ]
     },
-    add: function( html, tag ){  
-       DOM.RENDER[ html ] = [ tag, html ]
+    add: function( html, el ){  
+       DOM.RENDER[ html ] = [ el, html ]
        DOM.update( html )
     },
     getRender: ( name ) => DOM.RENDER[ name ],
-    update: async function( tag ){ 
-       var r = DOM.getRender( tag ) 
+    update: async function( el ){ 
+       var r = DOM.getRender( el ) 
        if( r ) { 
          DOM.draw( r[0], MOD.h( HTML[ r[1] ] ) )
-         console.log( "Updated path: "+tag )
+         console.log( "Updated path: "+el )
        }
     },
-    draw: ( tag, el ) => {       
-      if( !el ) DOM.none( tag )
-      else if( !UTILS.contains(el, "<undefined>" ) ) DOM.get( tag ).innerHTML = el
-      else DOM.none( tag )
+    draw: ( el, content ) => {       
+      if( !content ) DOM.none( el )
+      else if( !UTILS.contains( content, "<undefined>" ) ) DOM.get( el ).innerHTML = content
+      else DOM.none( el )
     },
-    none: ( tag ) => { 
-      DOM.get( tag ).innerHTML = " "
+    none: ( el ) => { 
+      DOM.get( el ).innerHTML = " "
     },
-    remove: function( tag ){ 
+    remove: function( id ){ 
       var el, root;     
-      if( UTILS.contains( tag, "_") ){        
-          el = tag.split("_")
+      if( UTILS.contains( id, "_") ){        
+          el = id.split("_")
           root = el[0]
           if( el.length == 5 ){
             el =  el[0]+"_"+el[1]+"_"+el[2]+"_"+el[3]            
             for( index in HTML[el] ){   
-              if( HTML[el][index].id == tag ) delete HTML[el][index]
+              if( HTML[el][index].id == id ) delete HTML[el][index]
             }
           }else if( el.length == 4 ){
             el =  el[0]+"_"+el[1]+"_"+el[2]            
             for( index in HTML[el] ){   
-              if( HTML[el][index].id == tag ) delete HTML[el][index]
+              if( HTML[el][index].id == id ) delete HTML[el][index]
             }
           }else if( el.length == 3 ){
             el =  el[0]+"_"+el[1]            
             for( index in HTML[el] ){   
-              if( HTML[el][index].id == tag ) delete HTML[el][index]
+              if( HTML[el][index].id == id ) delete HTML[el][index]
             }
           }else if( el.length == 2 ){
             el =  el[0]+"_"+el[1]
             delete HTML[el] 
             HTML[root].html = ""             
           }
-      }else delete HTML[ tag ]      
+      }else delete HTML[ id ]      
     }   
 }
 
 var MOD = {      
-    types: [ "input", "img", "area", "source", "embed" ],
+    emptyElements: [ "area","base","br","col","embed","hr","img","input",
+            "keygen","link","meta","param","source","track","wbr" 
+           ],
     h: function( data ){ 
       if( UTILS.isArray( data ) ) {   
         return MOD.mountMap( data )
       }else return data ? MOD.mount( data ) : 0
     },
     mount: function( data ){  
-      if( !data.type ) { data.type = "div" }
-      var el = "<"+data.type
+      if( !data.tag ) { data.tag = "div" }
+      var el = "<"+data.tag
       for( index in data.attrs ) { 
         if( UTILS.verify( index ) ) { el+= (" "+index+"='"+data.attrs[ index ]+"' ") } 
       }     
@@ -99,17 +101,17 @@ var MOD = {
       }else if( data.mountMap ) { 
         if( HTML[ data.mountMap ] ) el+= ">" + MOD.mountMap( HTML[ data.mountMap ] )
         else el+= ">" + MOD.mountMap( data.mountMap[0] ) 
-      }else if( MOD.types.includes( data.type ) ) { return el+"/>" }
+      }else if( MOD.emptyElements.includes( data.tag ) ) { return el+"/>" }
       else { el+=">" }
 
-      return el+="</"+data.type+">"
+      return el+="</"+data.tag+">"
     },
     mounts: function( v0, v1, v2 ){ 
       if( UTILS.isArray( v0 ) ){ var d=v0; v0=d[0]; v1=d[1]; v2=d[2]; }
       if( HTML[v2] ){ var t=HTML[v2]; v2 = MOD.h( t ) }
       else if( UTILS.isArray( v2 ) ){ var t=""; for( item in v2 ){ t+=v2[item]} v2=t; }
       var x = {
-        type: v0,
+        tag: v0,
         attrs: { ...v1 },
         html: v2,
       }  
@@ -119,14 +121,14 @@ var MOD = {
       var x = "";
       us.map(function( data, index ) {    
         if( us[ index ].mountMap ){ 
-          var t = [ us[ index ].type, { ...data }, "" ]
+          var t = [ us[ index ].tag, { ...data }, "" ]
           for( item in t[1]["mountMap"] ) t[2]+= MOD.mountMap( t[1]["mountMap"][0] ); 
           delete t[1].mountMap
           x+= MOD.mounts( t[0], t[1], t[2] ) 
         }else{
-          if( us[ index ].html ) x+= MOD.mounts( us[ index ].type, us[ index ], us[ index ].html )
+          if( us[ index ].html ) x+= MOD.mounts( us[ index ].tag, us[ index ], us[ index ].html )
           else if( us[ index ].mount ){
-            x+= MOD.mounts( us[ index ].type, us[ index ], MOD.mount( us[ index ].mount[0] ) )          
+            x+= MOD.mounts( us[ index ].tag, us[ index ], MOD.mount( us[ index ].mount[0] ) )          
           } 
         } 
       })   
@@ -136,8 +138,8 @@ var MOD = {
 
 var UTILS = {
     subs: function( str ){ return str.substring(1)  },
-    contains: function( tag, value ){  
-      if( tag.indexOf( value ) !== -1) return true
+    contains: function( el, value ){  
+      if( el.indexOf( value ) !== -1) return true
       else return false
     },
     removeThis: async function( html, symbol ){  
@@ -146,7 +148,7 @@ var UTILS = {
       return name
     },
     verify: ( value ) => {  
-      var array = [ "html", "mount", "mountMap", "mounts", "type" ]    
+      var array = [ "html", "mount", "mountMap", "mounts", "tag" ]    
       if( array.includes( value ) ) return false
       else return true
     },
@@ -166,4 +168,4 @@ HTML = ObservableSlim.create( OBS, true, function( changes ) {
     if( currentPath ) DOM.update( currentPath ) 
   }
 });  
-
+ 
